5562de00a3709fdf26a4dec1bce81827deeba65c
Sanal L S <sanalpayaswini@gmail.com>
2023-02-23 13:08:47 +0530

    Initial

--- training/imshow4.py
+++ training/imshow4.py
@@ -3,7 +3,7 @@ import cv2
 
 img = cv2.imread('image1.png')
 
-cv2.putText(img, "Python",(50,70),cv2.FONT_HERSHEY_DUPLEX,0.6,(0,255,0),2)
+cv2.putText(img, "Python",(50,70),cv2.FONT_HERSHEY_DUPLEX,0.6,(120,120,0),2)
 cv2.imshow("Image",img)
 
 cv2.waitKey()
