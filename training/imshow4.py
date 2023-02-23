c4afea64e72298da9014f4d6f13fcb5c4a93ac3f
Sanal L S <sanalpayaswini@gmail.com>
2023-02-23 13:15:48 +0530

    rectangle

--- training/imshow4.py
+++ training/imshow4.py
@@ -3,7 +3,8 @@ import cv2
 
 img = cv2.imread('image1.png')
 
-cv2.putText(img, "Python",(50,70),cv2.FONT_HERSHEY_DUPLEX,0.6,(120,120,0),2)
+cv2.putText(img, "Python",(50,70),cv2.FONT_HERSHEY_DUPLEX,2,(120,120,0),5)
+cv2.rectangle(img, (100,100), (200,50), (20,0,150), 3)
 cv2.imshow("Image",img)
 
 cv2.waitKey()
