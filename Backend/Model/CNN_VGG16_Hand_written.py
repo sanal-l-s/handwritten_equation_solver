b69a0c01457ee3147896d6d2393cfd0ec5f55996
AKHILA <akhilaammu0818@gmail.com>
2023-04-02 19:09:04 +0530

    plot loss and accuracy curve

--- Backend/Model/CNN_VGG16_Hand_written.py
+++ Backend/Model/CNN_VGG16_Hand_written.py
@@ -102,3 +102,38 @@ detection.metrics_names
 # here the the last epoch will be used.
 
 detection.evaluate_generator(validation_generator,steps=TRAIN_STEPS)
+
+# display the loss and accuracy curves
+
+import matplotlib.pyplot as plt
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.show()
+
+epochs
+
+loss
+
+val_loss
+
+acc
+
+val_acc
