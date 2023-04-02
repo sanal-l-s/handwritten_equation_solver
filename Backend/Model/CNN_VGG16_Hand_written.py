5dc0b8bebe6e8e700bb18ab9296740e0a3d2c5cf
Sanal L S <sanalpayaswini@gmail.com>
2023-04-02 18:33:10 +0530

    model training

---
+++ Backend/Model/CNN_VGG16_Hand_written.py
@@ -0,0 +1,104 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+import numpy as np 
+import matplotlib.pyplot as plt
+# here we are working on Tensorflow version 2.1.0 so we need to write tensorflow.keras.
+#keras is in built function in Tensorflow.
+import os
+import tensorflow
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Input, Dropout,Flatten, Conv2D
+from tensorflow.keras.layers import BatchNormalization, Activation, MaxPooling2D
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.utils import plot_model
+from IPython.display import SVG, Image
+
+train_location = "/content/drive/MyDrive/Hand Written/DataSet" 
+test_location = "/content/drive/MyDrive/Hand Written/DataSet"
+filepath = '/content/drive/MyDrive/Hand Written/CNN/VGG16/Model/Hand_written_VGG16_model1.h5'
+
+#from tensorflow.keras.models import load_model
+#Detection=load_model(filepath)
+
+preprocess_input = tensorflow.keras.applications.mobilenet.preprocess_input
+
+datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+img_size=224
+batch_size=25
+num_class=49
+
+# Complete Dataset images can be loaded using ImageDataGenerator function
+
+datagen_train=ImageDataGenerator(horizontal_flip=True)
+train_generator=datagen_train.flow_from_directory(train_location,target_size=(img_size,img_size),batch_size=batch_size,class_mode='categorical',shuffle=True)
+
+datagen_test=ImageDataGenerator(horizontal_flip=True)
+validation_generator=datagen_test.flow_from_directory(test_location,target_size=(img_size,img_size),batch_size=batch_size,class_mode='categorical',shuffle=True)
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+vgg = VGG16(input_shape=[img_size,img_size] + [3], weights='imagenet', include_top=False)
+
+vgg.summary()
+
+for layer in vgg.layers:
+  layer.trainable = False
+
+x = Flatten()(vgg.output)
+prediction = Dense(num_class, activation='softmax')(x)
+
+detection = Model(inputs=vgg.input, outputs=prediction)
+
+
+detection.summary()
+
+optimum=Adam(learning_rate=0.005)
+detection.compile(optimizer=optimum,loss='categorical_crossentropy',metrics=['accuracy'])
+
+
+print(train_generator.class_indices)
+
+
+TRAIN_STEPS=train_generator.n//train_generator.batch_size
+TRAIN_STEPS
+
+
+VALIDATION_STEPS=validation_generator.n//validation_generator.batch_size
+VALIDATION_STEPS
+
+checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+
+
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy',
+                              factor=0.5,
+                              patience=2, 
+                              verbose=1,
+                              mode='max',
+                              min_lr=0.00001)
+
+callbacks_list = [checkpoint, reduce_lr]
+#callbacks_list = [checkpoint]
+
+#history = detection.fit_generator(train_generator,
+history = detection.fit(train_generator,
+                              steps_per_epoch=TRAIN_STEPS, 
+                              #class_weight=class_weights,
+                              validation_data=validation_generator,
+                              validation_steps=VALIDATION_STEPS,
+                              epochs=5,
+                              verbose=1,
+                              callbacks=callbacks_list
+                             )
+
+# get the metric names so I can use evaulate_generator
+detection.metrics_names
+
+# here the the last epoch will be used.
+
+detection.evaluate_generator(validation_generator,steps=TRAIN_STEPS)
